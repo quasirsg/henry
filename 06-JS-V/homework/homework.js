211d86d2920773faf7cae7cd319d0523b41ca33a
Romano Javier <alexis_b13@live.com.ar>
2020-07-23 17:34:23 -0300

    tarea 6 completa

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -9,12 +9,27 @@ function crearUsuario() {
   // Devuelve la clase
   // Tu código:
 
+  function Usuario(opciones){
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password;
+  }
+  Usuario.prototype.saludar = function(){
+    return 'Hola, mi nombre es ' + this.nombre;
+  };
+
+  return Usuario;
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+
+  Constructor.prototype.saludar = function(){
+    return "Hello World!";
+  }
 
 }
 
@@ -24,6 +39,16 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+
+  String.prototype.reverse = function(){
+    var stringInvertida = '';
+
+    for (var i = this.length - 1  ;  i >= 0; i--) {
+      stringInvertida = stringInvertida + this.charAt(i);
+    }
+
+    return stringInvertida;
+  }
 
 }
 
